3b963e4a5f6156a772676cbe976b7884cf906786
shoeb <shoeb.ahmed@hotmail.com>
2017-04-17 18:37:27 -0500

    shoeb: mU matrix works with som_shoeb. Need to check with som_prash

--- src/som_shoeb.m
+++ src/som_shoeb.m
@@ -14,9 +14,9 @@ function som_shoeb
     fsuffix  = sprintf(repmat('_%d', 1, size(osuffix, 2)),osuffix );
     
     %run and collect results for problem2a
-%     prob2a(fsuffix);
-%     pause(2);
-%     close all;
+    prob2a(fsuffix);
+    pause(2);
+    close all;
 %    
 %     % run and collect results for problem2b and problem 3
 %     prob2b_and_3(fsuffix);
@@ -24,7 +24,7 @@ function som_shoeb
 %     close all;
 % 
 %     %run and collect results for problem4
-    prob4(fsuffix); 
+%     prob4(fsuffix); 
     display('Finished running all problems and saved figures/log reports.');
     pause(2);
     fclose('all');
@@ -125,40 +125,42 @@ function [W,total_iter] = prob2a(fsuffix)
         hold off;
     end
 
-%     fig = figure;
-%     fig = mesh2D_prototype_topology(fig,W,sprintf(['End of training, '...
-%         'total steps:%d'],total_iter),vw);
-%     fig = scatter2D_data_points(fig,X,D,sprintf(['End of training, '...
-%         'total steps:%d'],total_iter),vw);
-%     saveas(gcf,sprintf('%sfig%d_after_training_data_plot_%s.fig',...
-%         this_function,fig,fsuffix));
-% 
-%     fig = figure;
-%     whitebg('black');
-%     tstr = 'Problem 2a, Fence and weights after training';
-%     fig = plot_mU(fig,1-colormap(gray),lat_width,W,tstr);
-%     fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1]);
-%     saveas(gcf,sprintf(['%sfig%d_after_training_mU_matrix_and_weights_'...
-%         '%s.fig'],this_function,fig,fsuffix));
-% 
-%     fig = figure;
-%     tstr = 'Problem 2a, Class colors and weights';
-%     colmapcell = {[0 0 0],[0 0.251 0],[0.251 0 0.502],[1 0.502 0.502],...
-%         [0.502 0 0]};
-%     fig = decorate_class_color(fig,lattice,W,X,D,colmapcell,tstr,0.1);
-%     fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1]);
-%     saveas(gcf,sprintf(['%sfig%d_after_training_classfication_'....
-%         'colors_and_weights_%s.fig'],this_function,fig,fsuffix));
-% 
-%     fig = figure;
-%     tstr = 'Problem 2a, Fence, class color map, and weights';
-%     fig = plot_mU(fig,1-colormap(gray),lat_width,W,tstr);
-%     fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1]);
-%     fig = decorate_class_color(fig,lattice,W,X,D,colmapcell,tstr,5);
-%     saveas(gcf,sprintf('%sfig%d_after_training_all_overlaid_', ...
-%         this_function,fig,fsuffix));
-% 
-%     whitebg('white');
+    fig = figure;
+    fig = mesh2D_prototype_topology(fig,W,sprintf(['End of training, '...
+        'total steps:%d'],total_iter),vw);
+    fig = scatter2D_data_points(fig,X,D,sprintf(['End of training, '...
+        'total steps:%d'],total_iter),vw);
+    saveas(gcf,sprintf('%sfig%d_after_training_data_plot_%s.fig',...
+        this_function,fig,fsuffix));
+
+    fig = figure;
+    whitebg([0 0 0]);
+    tstr = 'Problem 2a, Fence and weights after training';
+    fig = plot_mU(fig,1-colormap(gray),lat_width,W,tstr,2);
+    fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1],1);
+    saveas(gcf,sprintf(['%sfig%d_after_training_mU_matrix_and_weights_'...
+        '%s.fig'],this_function,fig,fsuffix));
+
+    fig = figure;
+    whitebg([0 0 0]);
+    tstr = 'Problem 2a, Class colors and weights';
+    colmapcell = {[0 0 0],[0 0.251 0],[0.251 0 0.502],[1 0.502 0.502],...
+        [0.502 0 0]};
+    fig = decorate_class_color(fig,lattice,W,X,D,colmapcell,tstr,0.1);
+    fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1],1);
+    saveas(gcf,sprintf(['%sfig%d_after_training_classfication_'....
+        'colors_and_weights_%s.fig'],this_function,fig,fsuffix));
+
+    fig = figure;
+    whitebg([0 0 0]);
+    tstr = 'Problem 2a, Fence, class color map, and weights';
+    fig = decorate_class_color(fig,lattice,W,X,D,colmapcell,tstr,2);
+    fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1],1);
+    fig = plot_mU(fig,1-colormap(gray),lat_width,W,tstr,2);
+    saveas(gcf,sprintf('%sfig%d_after_training_all_overlaid_', ...
+        this_function,fig,fsuffix));
+
+    whitebg('white');
     fprintf('Total learning steps: %d\n',total_iter);
     display('Finished problem 2a');
 end
@@ -355,10 +357,10 @@ function [W,total_iter] = prob4(fsuffix)
     function fig = diag_plot(fig,W,tstr)
     % nested function. wrapper for plotting functions.
         fig = figure(fig);
-        whitebg('black');
+        whitebg([0 0 0]);
         class_color = {[0 0 0], [0 0 0], [0 0 0], [0 0 0]};
         fig = decorate_class_color(fig,lattice,W,X,D,class_color,tstr,2);
-        fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1]);
+        fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1],1);
         fig = plot_mU(fig,1-colormap(gray),lat_width,W,tstr,2);
         fig = decorate_class_label(fig,lattice,W,X,D,label_text,...
             label_color,4,tstr);
@@ -376,9 +378,9 @@ function [W,total_iter] = prob4(fsuffix)
     
     %% Process results after training.
     fig = figure;
-    whitebg('black');
+    whitebg([0 0 0]);
     tstr = 'Problem 4, Fence, weights and labels after training';
-    fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1]);
+    fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1],1);
     fig = plot_mU(fig,1-colormap(gray),lat_width,W,tstr,2);
     fig = decorate_class_label(fig,lattice,W,X,D,label_text,...
             label_color,4,tstr);
@@ -390,7 +392,7 @@ function [W,total_iter] = prob4(fsuffix)
     label_color = {[0 0 0], [1 1 1], [1 1 1], [1 1 1]};
     colmapcell  = {[0 0 0], [1 0 1], [0 1 1], [0 1 0]};
     fig = decorate_class_color(fig,lattice,W,X,D,colmapcell,tstr,0.1);
-    fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1]);
+    fig = decorate_weight_vector(fig,lattice,W,tstr,[1 1 1],1);
     fig = decorate_class_label(fig,lattice,W,X,D,label_text,...
             label_color,5,tstr);
     saveas(gcf,sprintf(['%sfig%d_after_training_classfication_'....
@@ -451,9 +453,8 @@ function [W,iter] = som_learn(X,M,h,eta,N,stop_predicate,som_logger, ...
     [mx,nx] = size(X);            % its better if pre-processing type is 
     X_mean = mean(X,2);           % also a parameter for som_learn.
     X_scale= max(abs(X(:)));
-    X      = bsxfun(@minus, X,X_mean);
-    X      = bsxfun(@rdivide, X,X_scale);% X is now normalized input
-    W = 2*rand(mx,M,'double')-1;         % initialialize weights.
+    X      = (X - repmat(X_mean,1,nx))/X_scale; % X is now normalized input 
+    W = (max(X(:))-min(X(:)))*rand(mx,M,'double')+min(X(:));% initialialize weights. 
     h('reset');                          % initialize neighborhood function
     mu=eta('reset');                     % initialize learning rate.    
     iter = 0;                            % total iterations counter.
@@ -462,9 +463,7 @@ function [W,iter] = som_learn(X,M,h,eta,N,stop_predicate,som_logger, ...
         for p = randperm(nx)
             % bokekeeping (call logger)
             if(mod(iter,log_step)==0)
-                W_denormalized = bsxfun(@times,W,X_scale); 
-                W_denormalized = bsxfun(@plus,W_denormalized,X_mean);
-                som_logger(iter,W_denormalized);      
+                som_logger(iter,W*X_scale + repmat(X_mean,1,M));      
             end
 %             i = (epoch-1)*N + p;
 % radius = initRadius * ((i <= decayIters/5) + .8 * (i > decayIters/5 & i <= decayIters/2) + .5 * (i > decayIters/2 & i <= decayIters*.8)+ .2 * (i > decayIters*.8));
@@ -475,7 +474,7 @@ function [W,iter] = som_learn(X,M,h,eta,N,stop_predicate,som_logger, ...
             end
             
             %2. find best match
-            Q = bsxfun(@(x,w)((x-w).^2),W,X(:,p));  % Euclidean distance
+            Q = (W - repmat(X(:,p),1,M)).^2;      % Euclidean distance 
             [~,c] = min(sum(Q,1));
             
             %3. update weights.
@@ -490,8 +489,7 @@ function [W,iter] = som_learn(X,M,h,eta,N,stop_predicate,som_logger, ...
         end
     end
 
-    W = bsxfun(@times,W,X_scale); 
-    W = bsxfun(@plus,W,X_mean);
+    W = W*X_scale + repmat(X_mean,1,M);
 end
 
 %% mU matrix
@@ -515,17 +513,20 @@ function fig = plot_mU(fig,fence_color,lat_wid,W,tstr,boundary_width)
     hdiff = Wpad(1:lat_wid+1,2:lat_len+1,:) - ...
         Wpad(2:lat_wid+2,2:lat_len+1,:);
     vdiff = Wpad(2:lat_wid+1,1:lat_len+1,:) - ...
-        Wpad(2:lat_wid+1,2:lat_len+2,:);    
+        Wpad(2:lat_wid+1,2:lat_len+2,:); 
     
-    hdiff = sum(hdiff.^2,3);    % find distance. hdiff : horiontal neighbors, 
-    vdiff = sum(vdiff.^2,3);    % vdiff for vertical.
+    % find distance. hdiff : horiontal neighbors, 
+    hdiff = sqrt(sum(hdiff.^2,3)./sum(Wpad(1:lat_wid+1,2:lat_len+1,:).^2,3));
+    
+    % vdiff for vertical.
+    vdiff = sqrt(sum(vdiff.^2,3)./sum(Wpad(2:lat_wid+1,1:lat_len+1,:).^2,3));
     [xg,yg] = meshgrid(1:lat_wid+1,1:lat_len+1);
     hdiff(isnan(hdiff)) = 0;
     vdiff(isnan(vdiff)) = 0;   
     scale = max([ max(abs(hdiff(:))), max(abs(vdiff(:))) ]);
     
-    hdiff = 1 - hdiff/scale;    % on gray scale, assume better matches 
-    vdiff = 1 - vdiff/scale;    % move to white.
+    hdiff = 1-hdiff/scale;    % on gray scale, assume better matches 
+    vdiff = 1-vdiff/scale;    % move to white.
     
     figure(fig);
     colormap(fence_color);
@@ -534,6 +535,7 @@ function fig = plot_mU(fig,fence_color,lat_wid,W,tstr,boundary_width)
     mesh(xg,yg,0*xg,hdiff,'EdgeColor','flat','LineWidth',boundary_width,...
         'FaceAlpha',0,'MeshStyle','row','Marker','none');
     hold on;
+    grid off;
     
     %plot vertical fences
     mesh(xg,yg,0*xg,vdiff,'EdgeColor','flat','LineWidth',boundary_width,...
@@ -587,9 +589,10 @@ function fig = decorate_class_color(fig,latf,W,X,D,class_color,tstr,...
  
     fig=figure(fig);
     hold on;
+    grid off;
     for i = 1:M
         ind = latf(i);
-        ver = [ind(1)+v(:,1),ind(2)+v(:,2)];
+        ver = [ind(2)+v(:,1),ind(1)+v(:,2)];
         fa = density_store(i) / max(abs(density_store(:)));
         patch('Faces',f,'Vertices',ver, 'FaceColor',...
             class_color{majority_class(i)},'FaceAlpha',fa,...
@@ -642,9 +645,10 @@ function fig = decorate_class_label(fig,latf,W,X,D,label,label_color,...,
  
     fig=figure(fig);
     hold on;
+    grid off;
     for i = 1:M
         ind = latf(i);
-        ver = [ind(1)+v(1),ind(2)+v(2)];
+        ver = [ind(2)+v(1),ind(1)+v(2)];
         text(ver(:,1), ver(:,2),label{majority_class(i)}, ...
             'Color',label_color{majority_class(i)},'FontSize',...
             label_fontsize);
@@ -654,7 +658,7 @@ function fig = decorate_class_label(fig,latf,W,X,D,label,label_color,...,
 end
 
 %% weight vectors
-function fig = decorate_weight_vector(fig,latf,W,tstr,line_color)
+function fig = decorate_weight_vector(fig,latf,W,tstr,line_color,line_width)
 %% Header
 % Decorate plot with weight vectors.  Plots the weight vectors in unit
 % squares whose corner coordinates are given by lattice 'latf' (see below).
@@ -674,11 +678,12 @@ function fig = decorate_weight_vector(fig,latf,W,tstr,line_color)
     v = zeros(M,2);
     figure(fig);
     hold on;
+    grid off;
     for i = 1:M
         v(i,:) = latf(i);
         y = (0.2)* (W(:,i)/max(abs(W(:,i)))) + 0.5;
-        plot(xval+v(i,1), y+v(i,2),...
-            'Color',line_color,'LineWidth',2);
+        plot(xval+v(i,2), y+v(i,1),...
+            'Color',line_color,'LineWidth',line_width);
     end
     title(tstr);
 end
